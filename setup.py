aa70b6a4d71527b84f3d146483aa518393b09af0
Ahmed Ibrahim <leondaz@macbook.local>
2023-05-22 00:10:48 +0300

    edit setup.py description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="exodia",
     version="1.0.0",
-    description="A client library for paymob API",
+    description="A client library for validation without hassle",
     author="LeOndaz",
     author_email="ahmeddark369@gmail.com",
     url="https://github.com/LeOndaz/exodia",
