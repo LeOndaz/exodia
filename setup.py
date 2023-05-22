1c251def7ec0b3de2f23f295bd798a362123276f
Ahmed Ibrahim <leondaz@macbook.local>
2023-05-22 15:51:20 +0300

    bump version - [no ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="exodia",
-    version="1.0.0",
+    version="1.0.1",
     description="A client library for validation without hassle",
     author="LeOndaz",
     author_email="ahmeddark369@gmail.com",
